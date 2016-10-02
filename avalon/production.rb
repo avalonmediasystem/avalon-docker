fe3e46a7a3b2f3787eff9d3b23fc35efcb298f2a
Michael Klein <mbklein@gmail.com>
2016-10-01 23:29:02 -0500

    Add in all known uncompiled assets

--- avalon/production.rb
+++ avalon/production.rb
@@ -5,7 +5,18 @@ Avalon::Application.configure do
   config.assets.compile = false
   config.assets.compress = true
   config.assets.digest = true
-  config.assets.precompile += %w( modernizr.js )
+  config.assets.precompile += %w( access_control_step.js android_pre_play.js autocomplete.js avalon_player.js 
+    avalon_playlists/playlist_items.js avalon_progress.js datatables_bootstrap.css dropdown_text_fields.js 
+    dynamic_fields.js embed.css embed_player.js file_browse.js file_table.js file_upload_step.js 
+    font-awesome.css import_button.js jasny-bootstrap.min.css jasny-bootstrap.min.js jquery-ui/datepicker.css 
+    jquery.dataTables.bootstrap.js jquery.dataTables.min.js jquery.xmleditor.css marker.js 
+    me-add-to-playlist.css me-add-to-playlist.js me-logo.css me-thumb-selector.css me-thumb-selector.js 
+    mediaelement-hd-toggle/mejs-hdtoggle.css mediaelement-hd-toggle/mep-feature-hdtoggle.js 
+    mediaelement-qualityselector/mep-feature-qualities.css mediaelement-qualityselector/mep-feature-qualities.js 
+    mediaelement-skin-avalon/mejs-skin-avalon.css mediaelement-skin-avalon/mep-feature-responsive.js 
+    mediaelement-title/mejs-title.css mediaelement_rails/mediaelement-and-player.js 
+    mediaelement_rails/mediaelementplayer.css modernizr.js select_all.js sort_streams.js xmleditor/ace.js 
+    xmleditor/jquery.xmleditor.js xmleditor/mode-xml.js xmleditor/vkbeautify.js xmleditor/xsd2json.js )
   config.cache_classes = true
   config.consider_all_requests_local = false
   config.eager_load = true
