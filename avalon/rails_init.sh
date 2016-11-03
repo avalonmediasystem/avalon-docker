91cd07915b732f2f361e782965c8998b00214b57
Phuong Dinh <phuongdh@gmail.com>
2016-11-03 15:17:19 -0400

    Support for resque

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -2,6 +2,10 @@
 chmod 0777 /masterfiles
 chown -R app /masterfiles
 cd /home/app/avalon
+su app
+BACKGROUND=yes QUEUE=* bundle exec rake resque:work
+BACKGROUND=yes bundle exec rake environment resque:scheduler
+exit
 cd public/assets/mediaelement_rails
 if [ ! -e flashmediaelement.swf ]; then
   ln -s flashmediaelement-*.swf flashmediaelement.swf
