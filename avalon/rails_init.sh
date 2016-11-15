bbbadae53986a1f2c00f1ad2dd7d1e40eda91f8f
Phuong Dinh <phuongdh@gmail.com>
2016-11-15 12:59:53 -0500

    Fix Redis settings

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -5,6 +5,7 @@ cd /home/app/avalon
 su app
 BACKGROUND=yes QUEUE=* bundle exec rake resque:work
 BACKGROUND=yes bundle exec rake environment resque:scheduler
+RAILS_ENV=production bundle exec rake db:migrate
 exit
 cd public/assets/mediaelement_rails
 if [ ! -e flashmediaelement.swf ]; then
