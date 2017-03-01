5051cff7d73c9f7c970e974d9bd85aafc83d69c1
Cloud User <ec2-user@ip-10-0-0-124.us-west-2.compute.internal>
2017-03-01 09:50:12 -0500

    Upgrade Fedora to 4.7.1 and use DB backend. Add sendmail to Avalon image

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+
+# sendmail needs this to work
+line=$(head -n 1 /etc/hosts)
+line2=$(echo $line | awk '{print $2}')
+echo "$line $line2.localdomain" >> /etc/hosts
+service sendmail start
+
 chmod 0777 -R /masterfiles
 chown -R app /masterfiles
 cd /home/app/avalon
+
 su app
 BACKGROUND=yes QUEUE=* bundle exec rake resque:work
 BACKGROUND=yes bundle exec rake environment resque:scheduler
 RAILS_ENV=production bundle exec rake db:migrate
 exit
+
 cd public/assets/mediaelement_rails
 if [ ! -e flashmediaelement.swf ]; then
   ln -s flashmediaelement-*.swf flashmediaelement.swf
