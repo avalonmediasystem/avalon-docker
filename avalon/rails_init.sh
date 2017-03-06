a80ad4aad178d79a3fddfb93fb445444d017ce8c
phuongdh <phuongdh@gmail.com>
2017-03-06 10:41:44 -0500

    Fix batch ingest cronjob not auto running

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -6,6 +6,9 @@ line2=$(echo $line | awk '{print $2}')
 echo "$line $line2.localdomain" >> /etc/hosts
 service sendmail start
 
+# batch ingest cronjob wouldn't autorun without this
+touch /var/spool/cron/crontabs/app
+
 chmod 0777 -R /masterfiles
 chown -R app /masterfiles
 cd /home/app/avalon
