a83151fe5c794e87ae5a476aeaca64c9a558065e
Phuong Dinh <phuongdh@gmail.com>
2018-03-07 12:12:19 -0500

    Add dev stack

---
+++ avalon/rails_init-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /home/app/avalon
+export HOME=/home/app/avalon
+bundle config build.nokogiri --use-system-libraries && \
+bundle install --path=/home/app/avalon/gems --with development test && \
+yarn install
+bundle exec rake assets:precompile
+
+rm -f tmp/pids/server.pid
+bundle exec rake db:migrate
+bundle exec rails server -b 0.0.0.0 -p 80 
+tail -f /dev/null
+
+# batch ingest cronjob wouldn't autorun without this
+#touch /var/spool/cron/crontabs/app
+
+#cd public/assets/mediaelement_rails
+#if [ ! -e flashmediaelement.swf ]; then
+#  ln -s flashmediaelement-*.swf flashmediaelement.swf
+#fi
