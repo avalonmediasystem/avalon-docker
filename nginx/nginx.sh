e505e4162efc771025989cdbc1485e69110fe36b
Chris Want <cjwant@gmail.com>
2018-06-04 13:10:45 -0600

    Make AVALON_DOMAIN and AVALON_STREAMING_PORT configurable for the hls container so that it can be used outside in situations where the avalon web server is not running in a container.

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-nginx
+
+# Make config file from template
+[ -z "$AVALON_DOMAIN" ] && AVALON_DOMAIN="http://avalon"
+[ -z "$AVALON_STREAMING_PORT" ] && AVALON_STREAMING_PORT=80
+export AVALON_DOMAIN
+export AVALON_STREAMING_PORT
+envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+exec /usr/local/nginx/sbin/nginx
