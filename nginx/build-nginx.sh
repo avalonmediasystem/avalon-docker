725d3793666c53a5db54f12d0fcbfc081f9bf5a2
Phuong Dinh <phuongdh@gmail.com>
2019-03-05 23:03:27 +0000

    Nginx works with public S3
    Nginx works with S3 and Avalon auth

--- nginx/build-nginx.sh
+++ nginx/build-nginx.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd /usr/src && apt-get source nginx && mv nginx-* nginx
-cd /usr/src && git clone https://github.com/arut/nginx-rtmp-module.git && cd nginx-rtmp-module && git checkout -b latest-tag $(git describe --tags)
 cd /usr/src && git clone https://github.com/kaltura/nginx-vod-module.git && cd nginx-vod-module && git checkout -b latest-tag $(git describe --tags)
+cd /usr/src && git clone https://github.com/anomalizer/ngx_aws_auth.git && cd ngx_aws_auth && git checkout -b latest-tag $(git describe --tags) 
 cd /usr/src/nginx
 ./configure --with-file-aio --with-cc-opt="-O3" --conf-path=/etc/nginx/nginx.conf \
-    --add-module=/usr/src/nginx-rtmp-module --add-module=/usr/src/nginx-vod-module \
-    --with-http_auth_request_module --with-http_sub_module \
-    --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log
+    --add-module=/usr/src/nginx-vod-module \
+    --add-module=/usr/src/ngx_aws_auth \
+    --with-http_auth_request_module --with-http_sub_module --with-http_ssl_module --with-debug \
+    --error-log-path=/dev/stderr --http-log-path=/dev/stdout
 make && make install
 cd /
 rm -rf /usr/src/*
--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -3,8 +3,10 @@
 # Make config file from template
 [ -z "$AVALON_DOMAIN" ] && AVALON_DOMAIN="http://avalon"
 [ -z "$AVALON_STREAMING_PORT" ] && AVALON_STREAMING_PORT=80
+[ -z "$AVALON_STREAMING_BUCKET" ] 
 export AVALON_DOMAIN
 export AVALON_STREAMING_PORT
-envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+export AVALON_STREAMING_BUCKET
+envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT,$AVALON_STREAMING_BUCKET' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec /usr/local/nginx/sbin/nginx
