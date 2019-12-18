9e9feb3564c95261dff01d9548e1ee928b7c0fa5
phuongdh <phuongdh@gmail.com>
2019-12-18 11:58:36 -0500

    Add worker container

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# sendmail needs this to work
-line=$(head -n 1 /etc/hosts)
-line2=$(echo $line | awk '{print $2}')
-echo "$line $line2.localdomain" >> /etc/hosts
-service sendmail start
-
 # batch ingest cronjob wouldn't autorun without this
 touch /var/spool/cron/crontabs/app
 
-chmod 0777 -R /masterfiles
-chown -R app /masterfiles
 cd /home/app/avalon
-
-su app
-BACKGROUND=yes QUEUE=* bundle exec rake resque:work
-BACKGROUND=yes bundle exec rake environment resque:scheduler
-RAILS_ENV=production bundle exec rake db:migrate
-exit
+RAILS_ENV=production su -m -c "bundle exec rake db:migrate" app
 
 tail -f /home/app/avalon/log/${RAILS_ENV}.log
