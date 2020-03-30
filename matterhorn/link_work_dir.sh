afcfb9f234503db743cf44b63046666967296bdc
phuongdh <phuongdh@gmail.com>
2020-03-30 11:08:03 -0400

    Use Puma-based Avalon image

--- matterhorn/link_work_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -d /usr/local/matterhorn/work ]; then
-  chmod 0777 /streamfiles
-  mkdir -p /work/work
-  chown matterhorn:matterhorn /work/work
-  su -c "ln -s /work/work /usr/local/matterhorn/work" matterhorn
-fi
