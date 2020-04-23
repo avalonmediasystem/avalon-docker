56467feb4df0a04016659d2ede04f06842e47d4f
phuongdh <phuongdh@gmail.com>
2020-04-23 14:48:35 -0400

    Add evs-s3helper

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Make config file from template
-[ -z "$AVALON_DOMAIN" ] && AVALON_DOMAIN="http://avalon"
+[ -z "$AUTH_URL" ] && AUTH_URL=http://avalon
 [ -z "$AVALON_STREAMING_PORT" ] && AVALON_STREAMING_PORT=80
-[ -z "$AVALON_STREAMING_BUCKET" ] 
-export AVALON_DOMAIN
+[ -z "$PROXY_PASS_URL" ] && PROXY_PASS_URL=http://s3-helper/
+export AUTH_URL
 export AVALON_STREAMING_PORT
-export AVALON_STREAMING_BUCKET
-envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT,$AVALON_STREAMING_BUCKET' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+export PROXY_PASS_URL
+envsubst '$AUTH_URL,$AVALON_STREAMING_PORT,$PROXY_PASS_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec /usr/local/nginx/sbin/nginx
