2d3434fe9bc0655db6a5e801d62c15046831ad7a
phuongdh <phuongdh@gmail.com>
2020-04-23 20:57:13 -0400

    Slim down Nginx image

--- nginx/build-nginx.sh
+++ nginx/build-nginx.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 cd /usr/src && apt-get source nginx && mv nginx-* nginx
-cd /usr/src && git clone https://github.com/arut/nginx-rtmp-module.git && cd nginx-rtmp-module && git checkout -b latest-tag $(git describe --tags)
 cd /usr/src && git clone https://github.com/kaltura/nginx-vod-module.git && cd nginx-vod-module && git checkout -b latest-tag $(git describe --tags)
 cd /usr/src/nginx
 ./configure --with-file-aio --with-cc-opt="-O3" --conf-path=/etc/nginx/nginx.conf \
-    --add-module=/usr/src/nginx-rtmp-module --add-module=/usr/src/nginx-vod-module \
+    --add-module=/usr/src/nginx-vod-module \
     --with-http_auth_request_module --with-http_sub_module \
     --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log
 make && make install
