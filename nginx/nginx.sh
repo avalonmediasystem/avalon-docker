747612212450742b00565688d6eb2bc97b0879bc
Chris Colvard <chris.colvard@gmail.com>
2022-05-09 14:08:36 -0400

    Nginx config for use in avalon development docker-compose environment

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -3,8 +3,10 @@
 # Make config file from template
 [ -z "$AVALON_DOMAIN" ] && AVALON_DOMAIN="http://avalon"
 [ -z "$AVALON_STREAMING_PORT" ] && AVALON_STREAMING_PORT=80
+[ -z "$AVALON_STREAMING_BUCKET_URL" ] && AVALON_STREAMING_BUCKET_URL="http://minio:9000/derivatives/"
 export AVALON_DOMAIN
 export AVALON_STREAMING_PORT
-envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+export AVALON_STREAMING_BUCKET_URL
+envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT,$AVALON_STREAMING_BUCKET_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec /usr/local/nginx/sbin/nginx
