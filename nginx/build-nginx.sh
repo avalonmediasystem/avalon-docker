c5655105f9c425095883cd3545ad4a55796e97dc
Chris Colvard <chris.colvard@gmail.com>
2023-05-17 11:44:21 -0400

    Rebuild using newer baseimage

--- nginx/build-nginx.sh
+++ nginx/build-nginx.sh
@@ -2,9 +2,9 @@
 cd /usr/src && apt-get source nginx && mv nginx-* nginx
 cd /usr/src && git clone https://github.com/kaltura/nginx-vod-module.git && cd nginx-vod-module && git checkout -b latest-tag $(git describe --tags)
 cd /usr/src/nginx
-./configure --with-file-aio --with-cc-opt="-O3" --conf-path=/etc/nginx/nginx.conf \
+./configure --with-file-aio --with-cc-opt="-O3 -mpopcnt -Wno-error=deprecated-declarations" --conf-path=/etc/nginx/nginx.conf \
     --add-module=/usr/src/nginx-vod-module \
-    --with-http_auth_request_module --with-http_sub_module \
+    --with-http_auth_request_module --with-http_sub_module --with-http_ssl_module \
     --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log
 make && make install
 cd /
